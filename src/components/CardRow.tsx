dac4e30206a2a653c22cf673ee3b923cd78c1a17
Abdurahmonov Jo'rabek <139786332+AJorabek@users.noreply.github.com>
2024-05-17 06:12:12 +0500

    Delete src directory

--- src/components/CardRow.tsx
+++
@@ -1,8 +0,0 @@
-import {ReactNode} from "react";
-
-function CardRow({name,type}:{type:ReactNode, name:ReactNode}) {
-    return <h1 className={"text-xl"}>{type}: <span
-        className={"text-blue-500 font-semibold capitalize"}>{name}</span></h1>
-}
-
-export default CardRow;
